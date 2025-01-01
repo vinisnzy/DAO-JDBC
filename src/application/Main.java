d95bdfa66ffbccb692d9ab6e159de94ce9b7ed26
Vinícius de Paula <manoseilacara123@gmail.com>
2025-01-01 15:53:02 -0300

    seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,14 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department department = new Department(1, "Computers");
-        System.out.println(department);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, department);
+        System.out.println(seller);
     }
 }
